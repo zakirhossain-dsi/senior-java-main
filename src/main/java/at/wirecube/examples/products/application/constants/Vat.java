e60cd5d9a62774ad301134f57957b04dcf44027e
Zakir <zakir@cefalo.com>
2022-09-25 09:59:26 +0600

    Added contextLoads test

--- src/main/java/at/wirecube/examples/products/application/constants/Vat.java
+++ src/main/java/at/wirecube/examples/products/application/constants/Vat.java
@@ -1,7 +1,13 @@
 package at.wirecube.examples.products.application.constants;
 
 public class Vat {
+
     public static final String TEN = "10";
     public static final String EIGHTEEN = "18";
     public static final String TWENTY = "20";
+
+    private Vat() {
+
+    }
+
 }
--- src/test/java/at/wirecube/examples/products/application/ProductManagementApplicationTests.java
+++ src/test/java/at/wirecube/examples/products/application/ProductManagementApplicationTests.java
@@ -2,12 +2,16 @@ package at.wirecube.examples.products.application;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class ProductManagementApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads(ApplicationContext context) {
+        assertNotNull(context);
     }
 
 }
