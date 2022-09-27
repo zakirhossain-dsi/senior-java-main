baf32d971bf836469cf50dd7a086d115c4c0db51
Zakir <zakir@cefalo.com>
2022-09-27 09:18:47 +0600

    Used built in spring security

--- src/main/java/at/wirecube/examples/products/application/security/WebSecurityConfig.java
+++ src/main/java/at/wirecube/examples/products/application/security/WebSecurityConfig.java
@@ -1,10 +1,7 @@
 package at.wirecube.examples.products.application.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -14,12 +11,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private static final String[] AUTH_WHITELIST = {
             "/swagger-resources/**", "/swagger-ui/*", "/v3/api-docs/**", "/webjars/**"
     };
-
-    @Value("${app.user}")
-    private String user;
-
-    @Value("${app.password}")
-    private String password;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -37,12 +28,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser(user.trim())
-                .password("{noop}" + password.trim())
-                .roles("USER");
-
-    }
 }
