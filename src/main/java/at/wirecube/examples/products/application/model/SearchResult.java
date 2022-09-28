c585ec174eeccdf94822638bc65cc085f55df8b2
Zakir <zakir@cefalo.com>
2022-09-28 16:59:35 +0600

    Added schemas on SearchResult

--- src/main/java/at/wirecube/examples/products/application/model/SearchResult.java
+++ src/main/java/at/wirecube/examples/products/application/model/SearchResult.java
@@ -1,5 +1,6 @@
 package at.wirecube.examples.products.application.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,9 +10,13 @@ import java.util.List;
 @Builder
 public class SearchResult<T> {
 
+    @Schema(example = "1")
     private long totalElements;
+    @Schema(example = "1")
     private long currentElements;
+    @Schema(example = "1")
     private int totalPages;
+    @Schema(example = "1")
     private int currentPage;
     private List<T> data;
 }
